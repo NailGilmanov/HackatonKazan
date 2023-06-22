71ea4643b1f3842df58910b956c4f97db1cc4d8f
NailGilmanov <nailgilmanov27@mail.ru>
2023-06-22 12:30:53 +0300

    history

--- js/main.js
+++ js/main.js
@@ -158,10 +158,14 @@ function load_module(url) {
 
             request_data('get_history/' + sessionStorage.getItem("uuid"), (d) => {
                 let wrap = document.querySelector('.analysis-history')
-                for (item in d) {
-                    console.log(item)
-                    wrap.innerHTML += '<div class="waste"><div class="left-part"><div class="waste--title">' + item['title'] + '</div><div class="waste--category">' + item['category'] + '</div></div><div class="right-part"><div class="waste--price removed">' + item["price"] + ' ₽</div><div class="waste--added-waste">Добавленная трата</div></div></div>'
-                }
+                d.forEach(element => {
+                    console.log(element)
+                    wrap.innerHTML += '<div class="waste"><div class="left-part"><div class="waste--title">' + element['title'] + '</div><div class="waste--category">' + element['category'] + '</div></div><div class="right-part"><div class="waste--price removed">' + element["price"] + ' ₽</div><div class="waste--added-waste">Добавленная трата</div></div></div>'
+                });
+                // for (item in d) {
+                //     console.log(item)
+                //     wrap.innerHTML += '<div class="waste"><div class="left-part"><div class="waste--title">' + item['title'] + '</div><div class="waste--category">' + item['category'] + '</div></div><div class="right-part"><div class="waste--price removed">' + item["price"] + ' ₽</div><div class="waste--added-waste">Добавленная трата</div></div></div>'
+                // }
             })
         }
     }, 400);
